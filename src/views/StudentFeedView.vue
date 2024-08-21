e0f712b72263556712ec0293c72424ea930722b2
eva <eva.pelko@gmail.com>
2024-08-21 03:22:09 +0200

    implement student feed posts from mongodb cluster

--- src/views/StudentFeedView.vue
+++ src/views/StudentFeedView.vue
@@ -10,7 +10,9 @@
 <script>
 import store from '../store';
 import PostCard from '../components/PostCard'
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+//import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { Posts } from "@/services";
+
 export default {
   name: 'StudentFeedView',
   data() {
@@ -24,6 +26,30 @@ export default {
   },
   methods: {
     async getPosts() {
+  try {
+    // Fetch posts from the backend using the Posts service
+    const studentPosts = await Posts.GetStudentFeed();
+    
+    // Map the posts data to the format expected by PostCard
+    const cards = studentPosts.map(post => ({
+      id: post._id,
+      url: `http://localhost:3000${post.url}`, // Prepend the backend URL to the image path
+      time: post.posted_at,
+      email: post.email,
+      title: post.title,
+      text: post.text,
+    }));
+
+    // Sort the cards array by time property in descending order
+    cards.sort((a, b) => new Date(b.time) - new Date(a.time));
+
+    this.cards = cards;
+  } catch (error) {
+    console.error("Error fetching student posts:", error);
+  }
+},
+
+    /* async getPosts() {
       const cards = [];
       const db = getFirestore();
       const querySnapshot = await getDocs(collection(db, "student-posts"));
@@ -45,7 +71,7 @@ export default {
       cards.sort((a, b) => b.time - a.time);
 
       this.cards = cards;
-    },
+    }, */
   },
   mounted() {
     this.getPosts();
